6e070f10cc7d0ce01782b591f35a88f6f34bb78a
sanjib mitra <sanjibmitra84@gmail.com>
2019-10-17 16:09:41 -0700

    2nd commit

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function showQuestion(question){
     question.answers.forEach(answer=>{
         const button=document.createElement('button');
         button.innerText=answer.text;
-        button.classList.add('.btn');
+        button.classList.add('btn');
         if(answer.correct){
             button.dataset.correct=answer.correct;
         }
